18292a61f368aef9cc1a6015e847614defa29e21
Mihai Luca <luca.mihai97@yahoo.com>
2022-04-30 14:01:57 +0300

    Add GET /captions tests

--- app/controllers/captions_controller.rb
+++ app/controllers/captions_controller.rb
@@ -1,5 +1,21 @@
 class CaptionsController < ApplicationController
   def index
-    head(200)
+    captions = [
+      {
+        id: 1,
+        url: "https://example.com/image.png",
+        text: "Hello World",
+        caption_url: ""
+      },
+      {
+        id: 124,
+        url: "",
+        text: "",
+        caption_url: ""
+      }
+    ]
+    render json:  {captions: captions}
   end
+
+  
 end
